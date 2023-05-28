626aeff11bb05d9f43dc5b2b1536123a7276ac1b
Yogesh Nagre <119948989+Yogeshnagre00@users.noreply.github.com>
2023-05-28 21:37:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const quizData =[
+const quizData = [
     {
         question: "Which language runs in a web browser?",
         a: "Java",
@@ -30,30 +30,30 @@ const quizData =[
         c: "1994",
         d: "none of the above",
         correct: "b",
-    }
-]
+    },
+];
 
+// const questions = [{},{},{},{}]
 
-const quiz = document.getElementById('quiz')
-const answerEls = document.querySelectorAll('.answer')
-const questionEl = document.getElementById('question')
-const a_text = document.getElementById('a_text')
-const b_text = document.getElementById('b_text')
-const c_text = document.getElementById('c_text')
-const d_text = document.getElementById('d_text')
-const submitBtn = document.getElementById('submit')
+const quiz = document.getElementById("quiz")
+const answerEls = document.querySelectorAll(".answer")
+const questionEl = document.getElementById("question")
 
-let currentQuiz = 0
+const submitBtn = document.getElementById("submit")
+
+const a_text = document.getElementById("a_text")
+const b_text = document.getElementById("b_text")
+const c_text = document.getElementById("c_text")
+const d_text = document.getElementById("d_text")
+
 let score = 0
 
-loadQuiz()
+let currentQuestion = 0
 
+function loadQuestion(){
 
-function loadQuiz() {
-    
-    deselectAnswer()
-
-    const currentQuizData = quizData[currentQuiz]
+    deselectAnswers()
+    const currentQuizData = quizData[currentQuestion]
 
     questionEl.innerText = currentQuizData.question
 
@@ -63,38 +63,41 @@ function loadQuiz() {
     d_text.innerText = currentQuizData.d
 }
 
-function deselectAnswer() {
-    answerEls.forEach(answerEl => answerEl.checked = false)
+function deselectAnswers(){
+    answerEls.forEach(answerEl=> answerEl.checked=false)
 }
 
-
 function getSelected(){
-    let answer
-    answerEls.forEach(answerEl => {
-        if(answerEl.checked) {
+    let answer;
+
+    answerEls.forEach(answerEl=>{
+        if(answerEl.checked){
             answer = answerEl.id
         }
     })
-
+    console.log(answer)
     return answer
 }
 
-submitBtn.addEventListener('click', () => {
+loadQuestion()
+
+submitBtn.addEventListener("click",()=>{
+
     const answer = getSelected()
 
-    if(answer) {
-        if(answer === quizData[currentQuiz].correct) {
-            score++
+    if(answer){
+        if(answer === quizData[currentQuestion].correct){
+            score++;
         }
+        currentQuestion++
 
-        currentQuiz++
-
-        if(currentQuiz < quizData.length) {
-            loadQuiz()
-        } else {
+        if(currentQuestion<quizData.length){
+            loadQuestion()
+        }else{
             quiz.innerHTML = `
-            <h2>\n You answered ${score}/${quizData.length} questions correctly\n</h2>
-
+            <h2>
+                You answered ${score}/${quizData.length} questions correctly
+            </h2>
             <button onclick="location.reload()">Reload</button>
             `
         }
